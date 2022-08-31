f2867a193850b4aac17e34bb89d853f187c0b2ad
unknown <ds3180459@gmail.com>
2022-08-31 16:51:15 +0700

    four github project

---
+++ luas segitiga.py
@@ -0,0 +1,10 @@
+def segitiga():
+
+    print("LUAS SEGITIGA")
+    print("=============")
+    a = float(input("Masukkan alas   :"))
+    t = float(input("Masukkan tinggi :"))
+    luas= 0.5 * a * t
+    print ("Luas segitiga =" ,luas, "cm2")
+
+segitiga()
